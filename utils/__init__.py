43d99b737425aaa1d41a580ab0b2d28139e8bb5c
roberta039 <165647793+roberta039@users.noreply.github.com>
2025-11-28 15:11:19 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,8 @@
 # Utils package initialization
-from .helpers import format_duration, format_price, get_airline_logo
-from .validators import validate_iata_code, validate_date, validate_passengers
+from .helpers import format_duration, format_price, get_stops_description
+from .validators import validate_iata_code, validate_date, validate_passengers, validate_search_params
 
 __all__ = [
-    'format_duration', 'format_price', 'get_airline_logo',
-    'validate_iata_code', 'validate_date', 'validate_passengers'
+    'format_duration', 'format_price', 'get_stops_description',
+    'validate_iata_code', 'validate_date', 'validate_passengers', 'validate_search_params'
 ]
