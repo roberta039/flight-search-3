052ba018d6c9980b78a053d3fb1e36d04adf71c6
roberta039 <165647793+roberta039@users.noreply.github.com>
2025-11-28 15:10:47 +0200

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,5 @@
 # Services package initialization
-from .flight_apis import FlightSearchService, AmadeusAPI, AirLabsAPI
+from .flight_apis import FlightSearchService, FlightOffer, SkyScrapperAPI, AirLabsAPI
 from .cache_manager import CacheManager
 
-__all__ = ['FlightSearchService', 'AmadeusAPI', 'AirLabsAPI', 'CacheManager']
+__all__ = ['FlightSearchService', 'FlightOffer', 'SkyScrapperAPI', 'AirLabsAPI', 'CacheManager']
